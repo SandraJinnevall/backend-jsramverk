875c537d3e7ed591bbbf9c4b69fc8b58434fcd34
SandraJinnevall <sandra.jinnevall@gmail.com>
2021-09-18 22:12:57 +0200

    travis

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
-const config = require("./config.json");
+// const config = require("./config.json");
 
-process.env.MONGOLINK = `mongodb+srv://${config.username}:${config.password}@cluster0.mc86y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// process.env.MONGOLINK = `mongodb+srv://${config.username}:${config.password}@cluster0.mc86y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 module.exports = {
     PORT: process.env.PORT || 1337,
-    mongoLink: process.env.MONGOLINK
 }
