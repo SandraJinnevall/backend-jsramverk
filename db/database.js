7ba41e5f9fdaf7f91d68b0689a0c12fbe9d0d8f1
SandraJinnevall <sandra.jinnevall@gmail.com>
2021-09-18 22:15:02 +0200

    travis

--- db/database.js
+++ db/database.js
@@ -4,7 +4,7 @@ const TestEditorDocument = require('../models/TestEditorDocument')
 // const config = require("./../config.json");
 const configjs = require("./../config.js");
 require('dotenv').config()
-let db = prosess.env.DB_LINK;
+let db = process.env.DB_LINK;
 // let db = `mongodb+srv://${config.username}:${config.password}@cluster0.mc86y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 let schema = EditorDocument;
 
