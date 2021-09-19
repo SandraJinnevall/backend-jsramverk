d9e503c44f8b527e073d864e621e0df1df2756a5
SandraJinnevall <sandra.jinnevall@gmail.com>
2021-09-19 14:20:35 +0200

    fix

--- db/database.js
+++ db/database.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose') //libary for mongodb.
 const EditorDocument = require('../models/EditorDocument')
 const TestEditorDocument = require('../models/TestEditorDocument')
-// const config = require("./../config.json");
 const configjs = require("./../config.js");
 require('dotenv').config()
 let db = process.env.DB_LINK;
-// let db = `mongodb+srv://${config.username}:${config.password}@cluster0.mc86y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 let schema = EditorDocument;
 
 if (process.env.NODE_ENV === 'test') {
