aa355fdd2b1533e276ffbb28add5ae28e870d023
SandraJinnevall <sandra.jinnevall@gmail.com>
2021-09-26 15:39:44 +0200

    sockets

--- server.js
+++ server.js
@@ -5,16 +5,43 @@ const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const editorDocumentRoutes = require('./routes/api/editorDocument');
+const http = require("http").Server(app);
+
+const socketio = require("socket.io")(http, {
+        cors: {
+        origin: "http://localhost:8080",
+        methods: ["GET", "POST"]
+    }
+});
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use(morgan('combined'))
 
 
-app.use('/api/editorDocument', editorDocumentRoutes)
 
-const server = app.listen(PORT, () => {
+socketio.sockets.on("connection", socket => {
+    socket.on('create', room => {
+        if (!room) {
+            room = socket.id;
+        }
+        socket.join(room);
+    });
+    socket.on("text", docData => {
+        if (!docData["_id"]) {
+            docData["_id"] = socket.id;
+        }
+        console.log("backend data", docData);
+        socket.to(docData["_id"]).emit("text", docData);
+    });
+});
+
+app.use('/', editorDocumentRoutes)
+
+const server = http.listen(PORT, () => {
     console.log('api listening on port ' + PORT);
 });
+
+
 
 module.exports = server;
