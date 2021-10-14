165e115cdcbc093b6401530adcca8c414d034d5d
SandraJinnevall <sandra.jinnevall@gmail.com>
2021-10-14 22:09:39 +0200

    testing pdf-route

--- routes/api/generatePDF.js
+++ routes/api/generatePDF.js
@@ -5,10 +5,17 @@ router.post('/', async (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
-    return res.pdfFromHTML({
-        filename: 'generated.pdf',
-        htmlContent: req.body.documentText
-    });
+    try {
+        var data = await res.pdfFromHTML({
+            filename: 'generated.pdf',
+            htmlContent: req.body.documentText
+        });
+        res.statusCode = 200;
+        return data
+    } catch (error) {
+        console.log('Need to be a html-string');
+        res.status(500).json({ message: error.message })
+    } 
 })
 
 module.exports = router
